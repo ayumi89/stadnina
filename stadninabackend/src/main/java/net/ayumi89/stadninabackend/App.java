2e631e5412745110c3f4aefb4bbc9800e2b2dcb1
Panna Sowka <agniesw@gmail.com>
2019-01-30 16:19:59 +0100

    1

---
+++ stadninabackend/src/main/java/net/ayumi89/stadninabackend/App.java
@@ -0,0 +1,13 @@
+package net.ayumi89.stadninabackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
